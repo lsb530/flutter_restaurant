f6bc25d06842d857da20e5b5528b96f976a2a572
boki <lsb530@naver.com>
2025-09-10 12:26:56 +0900

    RestaurantCard UI

---
+++ lib/feat/restaurant/component/restaurant_card.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_restaurant/common/const/colors.dart';
+
+class RestaurantCard extends StatelessWidget {
+  final Widget image;
+  final String name;
+  final List<String> tags;
+  final int ratingCount;
+  final int deliveryTime;
+  final int deliveryFee;
+  final double rating;
+
+  const RestaurantCard({
+    super.key,
+    required this.image,
+    required this.name,
+    required this.tags,
+    required this.ratingCount,
+    required this.deliveryTime,
+    required this.deliveryFee,
+    required this.rating,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(12.0),
+          child: image,
+        ),
+        const SizedBox(height: 16.0),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              name,
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            Text(
+              tags.join(' · '), // center dot
+              style: TextStyle(
+                color: BODY_TEXT_COLOR,
+                fontSize: 14.0,
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            Row(
+              children: [
+                _IconText(
+                  icon: Icons.star,
+                  label: rating.toString(),
+                ),
+                renderDot(),
+                _IconText(
+                  icon: Icons.receipt,
+                  label: ratingCount.toString(),
+                ),
+                renderDot(),
+                _IconText(
+                  icon: Icons.timelapse_outlined,
+                  label: '$deliveryTime 분',
+                ),
+                renderDot(),
+                _IconText(
+                  icon: Icons.monetization_on,
+                  label: deliveryFee == 0 ? '무료' : '$deliveryFee원',
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget renderDot() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4.0),
+      child: Text(
+        ' · ', // center dot
+        style: TextStyle(
+          fontSize: 12.0,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+    );
+  }
+}
+
+class _IconText extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const _IconText({
+    super.key,
+    required this.icon,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          icon,
+          color: PRIMARY_COLOR,
+          size: 14.0,
+        ),
+        const SizedBox(width: 8.0),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 12.0,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ],
+    );
+  }
+}
