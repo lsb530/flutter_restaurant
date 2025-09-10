0093542a09b9d0a89e7cf477cc9b7ad2e21abe69
boki <lsb530@naver.com>
2025-09-11 02:47:01 +0900

    mapping json data

--- lib/feat/restaurant/view/restaurant_screen.dart
+++ lib/feat/restaurant/view/restaurant_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_restaurant/common/const/data.dart';
 import 'package:flutter_restaurant/common/util/json_viewer.dart';
 import 'package:flutter_restaurant/feat/restaurant/component/restaurant_card.dart';
+import 'package:flutter_restaurant/feat/restaurant/model/restaurant_model.dart';
 
 class RestaurantScreen extends StatelessWidget {
   const RestaurantScreen({super.key});
@@ -34,7 +37,7 @@ class RestaurantScreen extends StatelessWidget {
             future: paginationRestaurant(),
             builder: (context, snapshot) {
               // print(snapshot.error);
-              // JsonViewer.printPretty(snapshot.data);
+              JsonViewer.printPretty(snapshot.data);
 
               if (!snapshot.hasData) {
                 return Container();
@@ -44,10 +47,24 @@ class RestaurantScreen extends StatelessWidget {
                 itemCount: snapshot.data!.length,
                 itemBuilder: (context, index) {
                   final item = snapshot.data![index];
+                  // p: parsed
+                  final pItem = RestaurantModel(
+                    id: item['id'],
+                    name: item['name'],
+                    thumbUrl: 'http://$hostPort${item['thumbUrl']}',
+                    tags: List<String>.from(item['tags']),
+                    priceRange: RestaurantPriceRange.values.firstWhere(
+                      (e) => e.name == item['priceRange'],
+                    ),
+                    ratings: item['ratings'],
+                    ratingsCount: item['ratingsCount'],
+                    deliveryTime: item['deliveryTime'],
+                    deliveryFee: item['deliveryFee'],
+                  );
 
                   return RestaurantCard(
                     image: Image.network(
-                      'http://$hostPort${item['thumbUrl']}',
+                      pItem.thumbUrl,
                       fit: BoxFit.cover,
                     ),
                     name: item['name'],
