4c73cf2dc685879290c0aeb7781bb601096cd5ed
boki <lsb530@naver.com>
2025-09-14 01:57:19 +0900

    CursorPagination JsonSerializable generic

--- lib/common/model/cursor_pagination_model.dart
+++ lib/common/model/cursor_pagination_model.dart
@@ -1,20 +1,23 @@
-import 'package:flutter_restaurant/feat/restaurant/model/restaurant_model.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'cursor_pagination_model.g.dart';
 
-@JsonSerializable()
-class CursorPagination {
+@JsonSerializable(
+  genericArgumentFactories: true,
+)
+class CursorPagination<T> {
   final CursorPaginationMeta meta;
-  final List<RestaurantModel> data;
+  final List<T> data;
 
   CursorPagination({
     required this.meta,
     required this.data,
   });
 
-  factory CursorPagination.fromJson(Map<String, dynamic> json)
-  => _$CursorPaginationFromJson(json);
+  factory CursorPagination.fromJson(
+    Map<String, dynamic> json,
+    T Function(Object? json) fromJsonT,
+  ) => _$CursorPaginationFromJson(json, fromJsonT);
 }
 
 @JsonSerializable()
