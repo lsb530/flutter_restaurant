0f6ea0b35892174017271896fe56486546be6817
boki <lsb530@naver.com>
2025-09-20 11:05:13 +0900

    fix RestaurantScreen according to CursorPaginationBase

--- lib/feat/restaurant/view/restaurant_screen.dart
+++ lib/feat/restaurant/view/restaurant_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_restaurant/common/model/cursor_pagination_model.dart';
 import 'package:flutter_restaurant/feat/restaurant/component/restaurant_card.dart';
 import 'package:flutter_restaurant/feat/restaurant/provider/restaurant_provider.dart';
 import 'package:flutter_restaurant/feat/restaurant/view/restaurant_detail_screen.dart';
@@ -11,18 +12,22 @@ class RestaurantScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final data = ref.watch(restaurantProvider);
 
-    if (data.isEmpty) {
+    if (data is CursorPaginationLoading) {
       return Center(
         child: CircularProgressIndicator(),
       );
     }
 
+    // cp: cursor pagination
+    final cp = data as CursorPagination;
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: ListView.separated(
-        itemCount: data.length,
+        itemCount: cp.data.length,
         itemBuilder: (context, index) {
-          final pItem = data[index];
+          final pItem = cp.data[index];
+
           return GestureDetector(
             onTap: () {
               Navigator.of(context).push(
