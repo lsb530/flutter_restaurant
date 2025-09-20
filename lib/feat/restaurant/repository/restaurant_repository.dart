0de2749d5e50ba6484c2ff29413f5867f80d390a
boki <lsb530@naver.com>
2025-09-20 11:33:09 +0900

    add Pagination params

--- lib/feat/restaurant/repository/restaurant_repository.dart
+++ lib/feat/restaurant/repository/restaurant_repository.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart' hide Headers;
 import 'package:flutter_restaurant/common/const/data.dart';
 import 'package:flutter_restaurant/common/dio/dio.dart';
 import 'package:flutter_restaurant/common/model/cursor_pagination_model.dart';
+import 'package:flutter_restaurant/common/model/pagination_params.dart';
 import 'package:flutter_restaurant/feat/restaurant/model/restaurant_detail_model.dart';
 import 'package:flutter_restaurant/feat/restaurant/model/restaurant_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -26,7 +27,9 @@ abstract class RestaurantRepository {
   @Headers({
     'accessToken': 'true',
   })
-  Future<CursorPagination<RestaurantModel>> paginate();
+  Future<CursorPagination<RestaurantModel>> paginate({
+    @Queries() PaginationParams? paginationParams = const PaginationParams(),
+  });
 
   @GET('/{id}')
   @Headers({
