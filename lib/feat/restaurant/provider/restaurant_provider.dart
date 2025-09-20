7f7e8c5b1e4e664a1ee0d0b4ec39a06a1d54feee
boki <lsb530@naver.com>
2025-09-20 11:04:41 +0900

    fix Provider response type, initial constructor data

--- lib/feat/restaurant/provider/restaurant_provider.dart
+++ lib/feat/restaurant/provider/restaurant_provider.dart
@@ -1,27 +1,26 @@
-import 'package:flutter_restaurant/feat/restaurant/model/restaurant_model.dart';
+import 'package:flutter_restaurant/common/model/cursor_pagination_model.dart';
 import 'package:flutter_restaurant/feat/restaurant/repository/restaurant_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final restaurantProvider =
-    StateNotifierProvider<RestaurantStateNotifier, List<RestaurantModel>>(
+    StateNotifierProvider<RestaurantStateNotifier, CursorPaginationBase>(
       (ref) {
         final repository = ref.watch(restaurantRepositoryProvider);
         return RestaurantStateNotifier(repository: repository);
       },
     );
 
-class RestaurantStateNotifier extends StateNotifier<List<RestaurantModel>> {
+class RestaurantStateNotifier extends StateNotifier<CursorPaginationBase> {
   final RestaurantRepository repository;
 
   RestaurantStateNotifier({
     required this.repository,
-  }) : super([]) {
+  }) : super(CursorPaginationLoading()) {
     paginate();
   }
 
   paginate() async {
     final resp = await repository.paginate();
-
-    state = resp.data;
+    state = resp;
   }
 }
