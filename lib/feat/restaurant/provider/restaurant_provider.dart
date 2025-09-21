3a4451a2a5fe7d93a0a917c568bac5b3e27ca924
boki <lsb530@naver.com>
2025-09-21 19:20:32 +0900

    add toJson to PaginationParams, paginate define 5 states on comment

--- lib/feat/restaurant/provider/restaurant_provider.dart
+++ lib/feat/restaurant/provider/restaurant_provider.dart
@@ -19,8 +19,19 @@ class RestaurantStateNotifier extends StateNotifier<CursorPaginationBase> {
     paginate();
   }
 
-  paginate() async {
-    final resp = await repository.paginate();
-    state = resp;
+  paginate({
+    int fetchCount = 20,
+    // 기존 데이터 유지 후, 추가로 데이터 요청
+    bool fetchMore = false,
+    // 강제로 전체 데이터 삭제 후, 다시 로딩(true: CursorPaginationLoading())
+    bool fetchRefetch = false,
+  }) async {
+    // 5가지 State
+    /// 1) CursorPagination - 정상적으로 데이터가 존재
+    /// 2) CursorPaginationLoading - 데이터 로딩중(현재 캐시 x)
+    /// 3) CursorPaginationError - 에러 발생
+    /// 4) CursorPaginationRefetching - 첫번째 페이지부터 다시 데이터 요청
+    /// 5) CursorPaginationFetchMore - 추가 데이터를 paginate로 받아오라는 요청
+    final x = 2;
   }
 }
