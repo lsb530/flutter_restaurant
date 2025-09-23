29aea2ce78bdfe72024efd545ec544f26da546d2
boki <lsb530@naver.com>
2025-09-23 17:50:27 +0900

    RestaurantRatingRepository

---
+++ lib/feat/restaurant/repository/restaurant_rating_repository.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_restaurant/common/model/cursor_pagination_model.dart';
+import 'package:flutter_restaurant/common/model/pagination_params.dart';
+import 'package:flutter_restaurant/feat/rating/model/rating_model.dart';
+import 'package:retrofit/http.dart';
+
+part 'restaurant_rating_repository.g.dart';
+
+// http://ip/restaurant/:rid/rating
+@RestApi()
+abstract class RestaurantRatingRepository {
+  factory RestaurantRatingRepository(Dio dio, {String baseUrl}) =
+      _RestaurantRatingRepository;
+
+  @GET('/')
+  @Headers({
+    'accessToken': 'true',
+  })
+  Future<CursorPagination<RatingModel>> paginate({
+    @Queries() PaginationParams? paginationParams = const PaginationParams(),
+  });
+}
