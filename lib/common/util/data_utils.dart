c631a203849480c2ced3fa3d9679f96b2b625949
boki <lsb530@naver.com>
2025-10-01 01:53:25 +0900

    add intl dependency, utils

--- lib/common/util/data_utils.dart
+++ lib/common/util/data_utils.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter_restaurant/common/const/data.dart';
+import 'package:intl/intl.dart';
 
 class DataUtils {
   static String pathToUrl(String value) {
@@ -14,5 +15,21 @@ class DataUtils {
   static String plainToBase64(String plain) {
     Codec<String, String> stringToBase64 = utf8.fuse(base64);
     return stringToBase64.encode(plain);
+  }
+
+  /// 숫자를 통화 형식으로 포맷
+  /// 기본값: locale=ko_KR, symbol=₩, decimalDigits=0
+  static String formatCurrency(
+      num value, {
+        String locale = 'ko_KR',
+        String symbol = '₩',
+        int decimalDigits = 0,
+      }) {
+    final formatter = NumberFormat.currency(
+      locale: locale,
+      symbol: symbol,
+      decimalDigits: decimalDigits,
+    );
+    return formatter.format(value);
   }
 }
